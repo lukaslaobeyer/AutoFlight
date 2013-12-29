92987ace22e944469b70d63e3f10fe07d1689dfc
Lukas Lao Beyer <lukaslaobeyer@gmail.com>
2013-12-29 17:43:42 +0100

    Cleaned repository (removed binaries, Advanced Installer cache files, etc.)

--- build_win/CMakeFiles/AutoFlight_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "AutoFlight_automoc.cpp"
-  "CMakeFiles/AutoFlight_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/AutoFlight_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
